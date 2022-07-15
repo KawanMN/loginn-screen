5465b3c08f92eb9f23d34d732c65faebbaf0fe9b
KawanMN <kawan15marques@gmail.com>
2022-07-15 09:37:49 -0300

    login_scren

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const inputs = document.querySelectorAll('.input');
 
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
+
+const handleFocusOut = ({ target }) => {
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+  }
+}
+
+
+inputs.forEach((inputs) => inputs.addEventListener('focus', handleFocus));
+inputs.forEach((inputs) => inputs.addEventListener('focusout', handleFocusOut));
+
